f8790c98e5828c316b8197d65c501d084811148d
Yamilla Calixto <myllacalixto13@gmail.com>
2023-03-16 17:50:21 -0300

    criando calculadora usando construtor functions

---
+++ javaScriptFuncoes/aula11/assets/js/main.js
@@ -0,0 +1,68 @@
+function Calculadora() {
+
+    this.display = document.querySelector('.display');
+
+    this.inicia = function () {
+        this.cliqueBotoes();
+        this.pressionaEnter();
+    }
+
+    this.clearDisplay = function () {
+        this.display.value = ' ';
+    }
+
+    this.apagaUm = function () {
+        this.display.value = this.display.value.slice(0,-1);
+    }
+
+    this.realizaConta = function () {
+        let conta = this.display.value;
+        try {
+            conta = eval(conta);
+            if (!conta){
+                alert('Conta inválida');
+                return;
+            }
+            this.display.value = String(conta);
+        } catch (error) {
+            alert('Conta inválida');
+            return;
+        }
+    }
+
+    this.pressionaEnter = function (){
+        this.display.addEventListener('keyup',(e) => {
+            if (e.keyCode === 13){
+                this.realizaConta();
+            }
+        }); 
+    }
+
+    this.cliqueBotoes = function () {
+        document.addEventListener('click', (e) => {
+            const elemento = e.target;
+            if (elemento.classList.contains('btn-num')) {
+                this.btnParaDisplay(elemento.innerText);
+            }
+            if (elemento.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+            if (elemento.classList.contains('btn-del')) {
+                this.apagaUm();
+            }
+            if (elemento.classList.contains('btn-eq')){
+                this.realizaConta();
+            }
+            this.display.focus();
+        });
+    }
+
+    this.btnParaDisplay = function (valor) {
+        this.display.value += valor;
+    }
+
+
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
