ec8378d87de94522a2317aadb0155018835bb4a1
Yamilla Calixto <myllacalixto13@gmail.com>
2023-06-27 16:14:57 -0300

    Estudando sobre Core-js e regenerator-runtime

---
+++ javaScriptTooling/aula06/src/modules/promises.js
@@ -0,0 +1,14 @@
+import { async } from "regenerator-runtime";
+
+function promise(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            console.log('Ei.sou a promise');
+            resolve();
+        },2000);
+    });
+}
+export default async function (){
+    await promise();
+    console.log('terminou');
+}
