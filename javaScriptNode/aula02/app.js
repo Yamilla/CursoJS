2c2c7c0a17809977bba0317872cfcf1eebb02acb
Yamilla Calixto <myllacalixto13@gmail.com>
2023-09-05 15:11:25 -0300

    estudando sobre modulos no node

---
+++ javaScriptNode/aula02/app.js
@@ -0,0 +1,7 @@
+// const multiplicacao = require('./mod');
+// console.log(multiplicacao(2,2));
+
+const path = require('path');
+console.log(__dirname);
+console.log(__filename);
+console.log(path.resolve(__dirname, '..','..','arquivos','imagens'));
---
+++ javaScriptNode/aula02/mod.js
@@ -0,0 +1,5 @@
+// estudando sobre modulos no node
+
+// module.exports = function (x,y){
+//     return x * y;
+// };
